48cab818568707307bed19d29aac4349d5b11e10
Varun <varunrajpurohit@hotmail.com>
2016-12-02 19:49:57 -0800

    First

--- Test/heroku_django_lti_helloworld/locator_cam_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from locator_cam_app.models import MomentPhoto, Moment, UserProfile, MomentThumbnail
-
-# Register your models here.
-admin.site.register(MomentPhoto)
-admin.site.register(Moment)
-admin.site.register(UserProfile)
-admin.site.register(MomentThumbnail)
--- Test/heroku_django_lti_helloworld/locator_cam_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocatorCamAppConfig(AppConfig):
-    name = 'locator_cam_app'
