6ea698d4397e52e0921a1a6abd686d451b00a801
Varun <varunrajpurohit@hotmail.com>
2016-12-05 10:29:55 -0800

    Latest DB Migration

--- locator_cam/settings.py
+++ locator_cam/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'locator_cam',
-    'south'
+    'locator_cam'
 ]
 
 MIDDLEWARE_CLASSES = [
