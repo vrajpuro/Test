f043d5ee96ecd7a3a1eea72701091f807593ad00
Varun <varunrajpurohit@hotmail.com>
2016-12-06 17:39:02 -0800

    UI Elements

--- locator_cam/urls.py
+++ locator_cam/urls.py
@@ -52,8 +52,8 @@ urlpatterns = patterns('',
 	url(r'^remove-administrator-from-channel/$', views.remove_administrator_from_channel, name='remove-administrator-from-channel'),
 	url(r'^delete-channel/$', views.delete_channel, name='delete-channel'),
 
-	                      )
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	                      ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += staticfiles_urlpatterns()
 
 
