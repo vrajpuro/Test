4a0fe15f4c3b12337224b0e9e1e765b2d50d8da1
Varun <varunrajpurohit@hotmail.com>
2016-12-06 17:32:06 -0800

    UI Elements

--- locator_cam/settings.py
+++ locator_cam/settings.py
@@ -142,6 +142,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'staticfiles'),
 
 # Heroku deployment settings
 
